57c8b60f0a987e53be3e54aea54ea0e3c2af4685
Erison <erivaps@gmail.com>
2018-10-15 19:45:12 -0500

    Initial commit

---
+++ app/src/main/java/com/kotlin/intents/intents02/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlin.intents.intents02
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun enviarSecondActivity(view: View){
+
+        var myIntent = Intent(this,SecondActivity::class.java)
+        startActivity(myIntent)
+    }
+}
